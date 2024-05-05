9910b844ce15db0f21a0549bc6169b359a675e68
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-05-05 20:56:12 -0300

    video chipi new origin

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from detect_colour import detect_colour
 from chewer import analyze_video as chewer_video
 from chipi import analyze_video as chipi_video
 
-detect_colour("videos/chewer-input.MOV", 20, 0.7)
-
+#detect_colour("videos/chewer-input.MOV", 20, 0.7)
+detect_colour("videos/chipi-input.MOV", 20, 0.7)
 #chewer_video()
 #chipi_video()
