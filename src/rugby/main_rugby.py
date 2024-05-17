cbdfb27b3a1a84b7f35f072331046c8220838ae3
Diego Villarroel <thedjv123@gmail.com>
2024-05-17 19:32:30 -0300

    Video de rugby convertido, todavía no encontré los valores apropiados

---
+++ src/rugby/main_rugby.py
@@ -0,0 +1,7 @@
+from src.rugby.process_video import process_video
+from src.general.detect_colour import detect_colour
+
+# obtiene la configuracion HSV para detectar la pelota
+detect_colour('rugby-chipi-input.mp4', 30)
+
+#process_video()
