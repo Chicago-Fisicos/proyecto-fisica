c75b811c580bdeed5e187ade9e1f3c161c12d102
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-05-21 19:34:56 -0300

    limpieza de videos q no usamos

--- src/rugby/main_rugby.py
+++
@@ -1,7 +0,0 @@
-from src.rugby.process_video import process_video
-from src.general.detect_colour import detect_colour
-
-# obtiene la configuracion HSV para detectar la pelota
-detect_colour('video-input.mp4', 30)
-
-#process_video()
