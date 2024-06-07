fb43ee7aefd62e1eefa503be1820b77bc0537436
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-06-07 15:45:33 -0300

    borre viejo analizador de audio

--- src/general/analizar_audio.py
+++ src/general/analizar_audio.py
@@ -1,12 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
-from pydub import AudioSegment
-
+import wave
 
 
 def main(ruta_archivo):
-
     # Cargar el archivo de audio
     audio, tasa_muestreo = cargar_audio(ruta_archivo)
 
@@ -17,8 +15,8 @@ def main(ruta_archivo):
     envolvente = np.abs(audio)
 
     # Detectar los picos (momentos de impacto)
-    umbral_minimo_picos = 1000
-    picos, _ = find_peaks(envolvente, height=umbral_minimo_picos, distance=tasa_muestreo * 0.1)
+    umbral_minimo_picos = 0.1 * np.max(envolvente)
+    picos, _ = find_peaks(envolvente, height=umbral_minimo_picos, distance=int(tasa_muestreo * 0.1))
 
     # Convertir los picos a tiempos en segundos
     tiempos = picos / tasa_muestreo
@@ -29,10 +27,12 @@ def main(ruta_archivo):
 
 
 def cargar_audio(ruta_archivo):
-    audio = AudioSegment.from_file(ruta_archivo)
-    muestras = np.array(audio.get_array_of_samples())
-    tasa_muestreo = audio.frame_rate
-    return muestras, tasa_muestreo
+    with wave.open(ruta_archivo, 'r') as archivo:
+        tasa_muestreo = archivo.getframerate()
+        num_frames = archivo.getnframes()
+        audio = archivo.readframes(num_frames)
+        audio = np.frombuffer(audio, dtype=np.int16)
+    return audio, tasa_muestreo
 
 
 def graficar(audio, envolvente, picos, tasa_muestreo, tiempos):
@@ -61,5 +61,5 @@ def coeficiente_de_restitucion(tiempos):
 
 
 if __name__ == "__main__":
-    ruta_audio = '../audios/Golf2.m4a'
+    ruta_audio = '../audios/Golf2.wav'
     main(ruta_audio)
