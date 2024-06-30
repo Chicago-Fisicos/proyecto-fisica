1d877d7801eac87b04ed79a6e5f3d491090a1d8b
Yoha <yohi.moyano@gmail.com>
2024-06-29 21:45:24 -0300

    teoria de errores de tiro doble completa

--- src/Teoria_Errores/teoria_error_tiro_doble.py
+++ src/Teoria_Errores/teoria_error_tiro_doble.py
@@ -37,9 +37,21 @@ def read_errors_curve_fit(file_path):
 
 def calculate_error_c_curve_fit():
     list_errors = read_errors_curve_fit(PATH_ERROR_C_CURVE_FIT)
-    c= list_errors[2][3]
-    error_c = list_errors[2][4]
-    return c*PIXEL_METROS, error_c*PIXEL_METROS
+    c_x = list_errors[2][1]
+    c_y= list_errors[2][3]
+    error_c_x = list_errors[2][2]
+    error_c_y = list_errors[2][4]
+
+    return c_x*PIXEL_METROS, c_y*PIXEL_METROS, error_c_x*PIXEL_METROS, error_c_y*PIXEL_METROS
+
+def calcular_valor_c(gravedad, vx):
+    c = (-1*gravedad)/((vx**2)*2)
+    return c
+
+def calcular_error_total_c():
+    c_x, c_y, error_c_x, error_c_y = calculate_error_c_curve_fit()
+    error_c_total = np.sqrt(error_c_x ** 2 + error_c_y ** 2)
+    return error_c_total
 
 def calculate_error_x():
     error_x = np.sqrt((PIXEL_METROS**2) + (MEASUREMENT_ERROR_METROS**2))
@@ -59,11 +71,11 @@ def calculate_error_velocidad_x (Xf,Xi,Tf,Ti):
     error_Vx = np.sqrt((derivadaVx_d**2) * (error_d**2) + (derivadaVx_t**2) * (error_t**2))
     return error_Vx
 
-def calculate_error_gravedad (c, error_c, error_Vx):
-    #gravedad = -2*c*(error_Vx**2)
+def calculate_error_gravedad (c, error_c, Vx, error_Vx):
+    #gravedad = -2*c*(Vx**2)
 
-    derivada_g_c = -4*error_Vx
-    derivada_g_v = -4*c
+    derivada_g_c = -2*(Vx**2)
+    derivada_g_v = -4*c*Vx
 
     error_gravedad = np.sqrt( (derivada_g_c**2) * (error_c**2) + (derivada_g_v**2) * (error_Vx**2) )
 
@@ -78,8 +90,9 @@ def extraer_datos ():
     Xi = data['X'].iloc[1]
     Tf = data['Time'].iloc[-1]
     Ti = data['Time'].iloc[1]
+    Vx = data['velocityX'].iloc[1]
 
-    return Xf,Xi,Tf,Ti
+    return Xf,Xi,Tf,Ti,Vx
 
 def calculate_g_prom ():
     # Leer los datos del archivo CSV
@@ -90,37 +103,59 @@ def calculate_g_prom ():
     g_prom = np.abs(np.mean(acceleration))
     return g_prom
 
+def calculate_error_velocidad_y(Xf,Xi,Tf, Ti, g_prom, error_x, error_g, error_t):
+    # Vy =  (d/t) + (g*(1/2)*(t))
+    t= Tf-Ti
+    d= Xf-Xi
+
+    derivada_Vy_d = 1 / t
+    derivada_Vy_g = 0.5 * t
+    derivada_Vy_t = (-d / t ** 2) + (0.5 * g_prom)
+
+    error_velocidad_y = np.sqrt((derivada_Vy_d ** 2) * (error_x ** 2) + (derivada_Vy_g ** 2) * (error_g ** 2) + (derivada_Vy_t ** 2) * (error_t ** 2) )
+    return error_velocidad_y
+
+
 def main():
     # Calculate_time_error
     error_time = calculate_time_error()
     print(f'El error del tiempo es: {error_time}')
 
     # Calculate_c_error
-    c, error_c = calculate_error_c_curve_fit()
-    print(f'C segun curve fit en metros es: {c}')
-    print(f'El error de C segun curve fit en metros es: {error_c}')
+    error_c_total = calcular_error_total_c()
+    print(f'El error de C segun curve fit en metros es: {error_c_total}')
 
     # Calculate_X_error
     error_x = calculate_error_x()
     print(f'El error de X es: {error_x}')
 
     # data
-    Xf, Xi, Tf, Ti = extraer_datos()
+    Xf, Xi, Tf, Ti, Vx = extraer_datos()
     print(f'Xf es: {Xf}')
     print(f'Xi es: {Xi}')
     print(f'Tf es: {Tf}')
     print(f'Ti es: {Ti}')
+    print(f'Vx es: {Vx}')
 
-    # Calculate_error_velocidad_x (Xf,Xi,Tf,Ti)
-    error_Vx = calculate_error_velocidad_x(Xf, Xi, Tf, Ti)
-
-    #calcular error de gravedad
-    error_gravedad = calculate_error_gravedad(c, error_c, error_Vx)
-    print(f'El error de G es: {error_gravedad}')
-
-    # calcular gravedad promedio
+    # calculate gravity(prom)
     g_prom = calculate_g_prom()
     print(f'El G promedio es: {g_prom}')
+
+    # calculate C
+    c_total = calcular_valor_c(g_prom, Vx)
+    print(f'El valor C en metros es: {c_total}')
+
+    # Calculate_error_velocity_x
+    error_Vx = calculate_error_velocidad_x(Xf, Xi, Tf, Ti)
+    print(f'El error de Vx es: {error_Vx}')
+
+    # calculate error gravity
+    error_gravedad = calculate_error_gravedad(c_total, error_c_total, Vx, error_Vx)
+    print(f'El error de G es: {error_gravedad}')
+
+    # calculate error in velocity_y
+    error_velocidad_y = calculate_error_velocidad_y(Xf, Xi, Tf, Ti, g_prom, error_x, error_gravedad, error_time)
+    print(f'El error de Vy es: {error_velocidad_y}')
 
     #Generar tabla
 
@@ -128,7 +163,7 @@ def main():
     tabla_de_Errores['Gravedad Error'] = error_gravedad
     tabla_de_Errores['Gravedad Promedio + Error'] = g_prom + error_gravedad
     tabla_de_Errores['Gravedad Promedio - Error'] = g_prom - error_gravedad
-    tabla_de_Errores.round(2).to_csv('Tabla_de_Errores.csv',index=False)
+    tabla_de_Errores.round(2).to_csv('Tabla_de_Errores_Tiro_Doble.csv',index=False)
 
 if __name__ == "__main__":
     main()
